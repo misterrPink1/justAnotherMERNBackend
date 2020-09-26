fdc7a5f6ef414eb4440174c1e4ca5ff6027addf2
RikeshThapa <rthapa@u.rochester.edu>
2020-09-25 19:19:16 -0500

    Adding POST create place

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,8 @@
+const { v4:uuid } = require("uuid");
+
 const HttpError = require('../models/http-error');
+
+
 
 const DUMMY_PLACES = [
     {
@@ -42,5 +46,22 @@ const getPlaceByUserId = (req, res, next) => {
 
 };
 
+
+const createPlace = (req, res, next) => {
+    const { title, description, coordinates, address, creator } = req.body;
+    const createdPlace = {
+        id: uuid(),
+        title,
+        description,
+        location: coordinates,
+        address,
+        creator
+    };
+
+    DUMMY_PLACES.push(createdPlace);
+    res.status(201).json({place: createdPlace});
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
