a05f9dce80cca1e011a188c80048cbe0c05bda5c
RikeshThapa <rthapa@u.rochester.edu>
2020-10-04 22:43:54 -0500

    adding error handling and validation

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const { v4:uuid } = require("uuid");
+const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
 //const { delete } = require("../routes/places-routes");
@@ -55,6 +56,13 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    }
+
+
     const { title, description, coordinates, address, creator } = req.body;
     const createdPlace = {
         id: uuid(),
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { check } = require('express-validator');
+
 const placesControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
@@ -10,7 +12,18 @@ router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
-router.post('/', placesControllers.createPlace);
+router.post('/',
+            [
+                check('title')
+                    .not()
+                    .isEmpty(),
+                check('description').isLength({min: 5}),
+                check('address')
+                    .not()
+                    .isEmpty() 
+            ],
+            placesControllers.createPlace
+        );
 
 router.patch('/:pid', placesControllers.updatePlace);
 
